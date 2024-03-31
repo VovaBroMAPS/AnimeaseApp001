39a5e2cbd85048394a0535102eff9935b8e537ae
Vovah <spivakvova2@gmail.com>
2024-03-31 13:28:37 +0300

    project list yayyy

---
+++ app/src/main/java/com/example/animeaseapp001/ProjectListFragment.java
@@ -0,0 +1,37 @@
+package com.example.animeaseapp001;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProjectListFragment extends Fragment {
+
+    public ProjectListFragment() {
+
+    }
+    public static ProjectListFragment newInstance(String param1, String param2) {
+        ProjectListFragment fragment = new ProjectListFragment();
+        Bundle args = new Bundle();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_project_list, container, false);
+    }
+}
